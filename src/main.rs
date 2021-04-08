c509fc174b5b65a73f1e2474dcb9938e61a41e28
Jeremy Thorne <jez@jezandbecks.co.uk>
2021-04-08 13:56:10 +0100

    true / false positive statistics
    use the known rotation between source and dest images to calculate
    whether a match is a true or false positive. Print histograms of true /
    false positiives and calculate mean and standard deviation.

--- src/main.rs
+++ src/main.rs
@@ -37,20 +37,81 @@ fn draw_matches(image:&mut RgbaImage, corners:&Vec<Corner>, matches:&Vec<Option<
     }
 }
 
-fn match_stats(corners:&Vec<Corner>, matches:&Vec<Option<&Corner>>) {
-    let mut distances = HashMap::<u32, u32>::new();
+fn expected_location(w:u32, h:u32, theta:f32, x:u32, y:u32) -> (u32, u32) {
+    let rot = na::Rotation2::new(-theta);
+    let trans1 = na::Translation2::new(-(w as f32 / 2.0), -(h as f32 / 2.0));
+    let trans2 = na::Translation2::new(w as f32 / 2.0, h as f32 / 2.0);
+    let transform = trans2 * rot * trans1;
+    let expected = transform * na::Point2::new(x as f32, y as f32);
+    (expected.x.round() as u32, expected.y.round() as u32)
+}
+
+fn match_stats(corners:&Vec<Corner>, matches:&Vec<Option<&Corner>>,
+               transform: (u32, u32, f32)) {
+    let (w, h, theta) = transform;
+
+    struct Stats {
+        distances: Vec<u32>
+    };
+    
+    let mut tp_distances = 
+        Stats {
+            distances: Vec::<u32>::new()
+        };
+    let mut fp_distances = 
+        Stats {
+            distances: Vec::<u32>::new()
+        };
+
     for (corner, omatch) in corners.iter().zip(matches.iter()) {
         if let Some(m) = omatch {
             let a = corner.descriptor.unwrap();
             let b = m.descriptor.unwrap();
             let d = hamming_lsh::hamming_distance(a, b);
-            let count = distances.entry(d / 5).or_insert(1);
-            *count += 1;
+            let e = expected_location(w, h, theta, corner.corner.x, corner.corner.y);
+            let true_positive = (e.0 as i32 - m.corner.x as i32).abs() < 2 
+                             && (e.1 as i32 - m.corner.y as i32).abs() < 2;
+            let stats = if true_positive { &mut tp_distances } else { &mut fp_distances };
+            stats.distances.push(d);
         }
     }
     println!("histogram of hamming distances between matches and original features");
-    for k in distances.keys().sorted() {
-        println!("distance {} - {} : {}", k * 5, (k + 1) * 5, distances.get(k).unwrap());
+    println!("true positives:");
+    calc(&tp_distances);
+    println!("false positives:");
+    calc(&fp_distances);
+
+    fn calc(stats:&Stats) {
+        let mut distances = HashMap::<u32, u32>::new();
+        for d in stats.distances.iter() {
+             let count = distances.entry(d / 5).or_insert(1);
+            *count += 1;  
+        }
+        for k in distances.keys().sorted() {
+            println!("distance {} - {} : {}", k * 5, (k + 1) * 5, distances.get(k).unwrap());
+        }
+        let mean = stats.distances.iter().fold(0.0, |s, d| s + *d as f32) / stats.distances.len() as f32;
+        let var = stats.distances.iter().fold(0.0, |v, d| v + (*d as f32 - mean) * (*d as f32 - mean)) / stats.distances.len() as f32;
+        let stddev = var.sqrt();
+        println!("mean: {}, stddev: {}", mean, stddev);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_expected_location() {
+        let w = 20;
+        let h = 20;
+        // no movement if theta = 0
+        assert_eq!(expected_location(w, h, 0.0, 5, 5), (5, 5));
+        // centre doesn't move
+        assert_eq!(expected_location(w, h, 0.1, w / 2, h / 2), (10, 10));
+        // pi / 2 => clockwise rotation about centre
+        let theta = std::f32::consts::PI / 2.0;
+        assert_eq!(expected_location(w, h, theta, 16, 12), (12, 4));
     }
 }
 
@@ -84,9 +145,12 @@ fn main() {
                             geometric_transformations::Interpolation::Nearest,
                             Luma([0]));
 
+    println!("finding features and matches in a copy rotated by {} degrees",
+             theta * 180.0 / std::f32::consts::PI);
 
     // find ORB corners in both and matches between the pair
-    let config = Config::default();
+    let mut config = Config::default();
+    config.lsh_max_distance = 128;
     
     let corners = find_multiscale_features(&src_image, &config);
 
@@ -99,7 +163,7 @@ fn main() {
     let corners_r = find_multiscale_features(&im_r, &config);
     let matches = find_matches(&corners, &corners_r, &config);
  
-    match_stats(&corners_r, &matches);
+    match_stats(&corners_r, &matches, (w, h, theta));
 
     let mut dst = src_image.expand_palette(&palette, None);
     draw_features(&mut dst, &corners);
